dbb769b9a43844168bef6ad2d1051c6a9f422467
Roman Voronin <zeromistakes4@gmail.com>
2024-07-23 10:46:19 +0200

    added project folder structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+function App() {
+  return (
+    <>
+    {/*  TODO HEADER*/}
+    {/*  TODO INPUT*/}
+    {/*  TODO LIST*/}
+    {/*  TODO FOOTER*/}
+      123
+    </>
+  )
+}
+
+export default App
