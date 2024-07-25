5a2e34344efc3d5d1275736248d5880cd0e36f75
Roman Voronin <zeromistakes4@gmail.com>
2024-07-25 20:58:07 +0200

    added light and dark theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,16 @@ import styles from './App.module.scss'
 import TodoHeader from "./components/TodoHeader/TodoHeader.tsx";
 import TodoInput from "./components/TodoInput/TodoInput.tsx";
 import TodoList from "./components/TodoList/TodoList.tsx";
+import useTodoStore from "./store/useTodoStore.ts";
+import {useEffect} from "react";
 function App() {
+
+  const {theme} = useTodoStore();
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+  
   return (
     <div className={styles.appWrapper}>
       <div className={styles.bgWrapper}/>
